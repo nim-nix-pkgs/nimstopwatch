c6ac04e81a969d9fc3ef304426d95941c0d5b506
hugosenari <hugosenari@gmail.com>
2022-03-27 21:36:29 -0300

    chore: re index nimstopwatch

--- master/flake.nix
+++ master/flake.nix
@@ -11,6 +11,7 @@
   inputs.src-nimstopwatch-master.ref   = "refs/heads/master";
   inputs.src-nimstopwatch-master.owner = "twist-vector";
   inputs.src-nimstopwatch-master.repo  = "nim-stopwatch";
+  inputs.src-nimstopwatch-master.dir   = "";
   inputs.src-nimstopwatch-master.type  = "github";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
